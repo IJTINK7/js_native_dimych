5233401e699522630861b18c31108a8882ceb0a6
IJTINK7 <ijtink7@mail.ru>
2023-08-31 16:34:13 +0300

    Creating 01.test.ts file

---
+++ src/01-tests/01.test.ts
@@ -0,0 +1,12 @@
+export const value01_01 = ""
+
+export const sum = (a: number, b: number)=>{
+	return a + b;
+}
+export const multimply = (a: number, b: number)=>{
+	return a * b;
+}
+export const splitIntoWords = (string: string) => {
+	return string.split(" ")
+}
+console.log(splitIntoWords("Hello my friend"))
