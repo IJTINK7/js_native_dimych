a67f59e2935849d204e6db2866f888a6517c9554
IJTINK7 <ijtink7@mail.ru>
2023-09-14 18:08:58 +0300

    Preparing for the array destructuring

--- src/07-destructuring_assignment/07.test.ts
+++ src/07-destructuring_assignment/07.test.ts
@@ -1,6 +1,43 @@
 import {StudentType} from "../02-objects/02";
 
+let student: StudentType;
+
+beforeEach(()=>{
+	student = {
+		name: "Roma",
+		age: 28,
+		isMarried: false,
+		address:{
+			country:"Belarus",
+			city: {
+				name: "Minsk",
+				street: "Surganova",
+				building: 2
+			}
+		},
+		technologies:[
+			{id: 1, title: "HTML"},
+			{id: 2, title: "CSS"},
+			{id: 3, title: "JS"},
+		]
+	}
+})
+
 test("Check the correct object values",()=>{
+	let{name, age, isMarried} = student
+	let{name: myName, age: newAge} = student // pseudonyms for variables (not typification !!! - not recommended)
+	console.log(myName)
+	console.log(newAge)
+
+	let technologies = student.technologies
+
+
+	expect(age).toBe(28)
+	expect(technologies.length).toBe(3)
+	expect(name).toBe("Roma")
+	expect(isMarried).toBe(false)
+})
+test("Check the correct array values",()=>{
 	let student: StudentType = {
 		name: "Roma",
 		age: 28,
