8198265ca7bca3ef08b78eeff98b699a8caf0aae
IJTINK7 <ijtink7@mail.ru>
2023-09-14 14:57:02 +0300

    Training "onBlur" event

--- src/06-callback_events/06.tsx
+++ src/06-callback_events/06.tsx
@@ -13,11 +13,15 @@ export const User = () =>{
 	const onNameChanged =()=>{
 		console.log("Name was changed")
 	}
+	const focusHasBeenLostHandler =()=>{
+		console.log("Focus Has Been Lost")
+	}
 	return (
 		<div>
 			<div>
-				<textarea onChange={onNameChanged}>Roma</textarea>
+				<textarea onChange={onNameChanged} onBlur={focusHasBeenLostHandler}>Roma</textarea>
 			</div>
+			<input type="text"/>
 			<button onClick={saveUser}>Save</button>
 			<button onClick={deleteUser}>Delete</button>
 			<div onClick={saveUser}>Save</div>
