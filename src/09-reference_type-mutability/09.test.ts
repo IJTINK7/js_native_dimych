a904eb022d87eb3d342ba3848f127ee48b7f6114
IJTINK7 <ijtink7@mail.ru>
2023-09-28 18:16:54 +0300

    Testing object and array mutable changing

--- src/09-reference_type-mutability/09.test.ts
+++ src/09-reference_type-mutability/09.test.ts
@@ -10,5 +10,31 @@ test("age should be changed", ()=>{
 	}
 	increaseAge(user)
 	expect(user.age).toEqual(29)
+	const superman = user;
+	superman.age = 1000
+	console.log(user)
+	console.log(superman)
+	expect(user.age).toEqual(1000)
 
 })
+test("array test", ()=>{
+	let users: UserType[] = [
+		{
+			name: "Roma",
+			age: 28
+		},
+		{
+			name: "Anton",
+			age: 30
+		},
+	]
+	let admins = users
+
+	admins.push({
+		name: "Kid",
+		age: 10
+	})
+
+	expect(users.length).toEqual(3)
+})
+
