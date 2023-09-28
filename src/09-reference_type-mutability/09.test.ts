aaba543e283f28442b9b70a66ad46d8010ada0e7
IJTINK7 <ijtink7@mail.ru>
2023-09-28 18:32:00 +0300

    Changing structure of objects

--- src/09-reference_type-mutability/09.test.ts
+++ src/09-reference_type-mutability/09.test.ts
@@ -3,10 +3,13 @@ import {UserType} from "./09";
 const increaseAge = (user: UserType) => {
 	user.age = user.age + 1
 }
-test("age should be changed", ()=>{
+test("age should be changed", () => {
 	let user: UserType = {
 		name: "Roma",
-		age: 28
+		age: 28,
+		address: {
+			title: "Minsk"
+		}
 	}
 	increaseAge(user)
 	expect(user.age).toEqual(29)
@@ -17,24 +20,55 @@ test("age should be changed", ()=>{
 	expect(user.age).toEqual(1000)
 
 })
-test("array test", ()=>{
+test("array test", () => {
 	let users: UserType[] = [
 		{
 			name: "Roma",
-			age: 28
+			age: 28,
+			address: {
+				title: "Minsk"
+			}
 		},
 		{
 			name: "Anton",
-			age: 30
+			age: 30,
+			address: {
+				title: "Minsk"
+			}
 		},
 	]
 	let admins = users
 
 	admins.push({
 		name: "Kid",
-		age: 10
+		age: 10,
+		address: {
+			title: "Minsk"
+		}
 	})
 
 	expect(users.length).toEqual(3)
 })
 
+test("array test 2", () => {
+	let users: UserType[] = [
+		{
+			name: "Roma",
+			age: 28,
+			address: {
+				title: "Minsk"
+			}
+		},
+		{
+			name: "Anton",
+			age: 30,
+			address: {
+				title: "Minsk"
+			}
+		},
+	]
+	let addr = users[0].address
+	let user2Name = addr.title
+	console.log(user2Name)
+	expect(user2Name).toEqual("Minsk")
+})
--- src/09-reference_type-mutability/09.ts
+++ src/09-reference_type-mutability/09.ts
@@ -1,10 +1,14 @@
 export type UserType = {
 	name: string
 	age: number
+	address: {title: string}
 }
 let user: UserType = {
 	name: "Roma",
-	age: 28
+	age: 28,
+	address: {
+		title: "Minsk"
+	}
 }
 const increaseAge = (user: UserType) => {
 	return user.age++
