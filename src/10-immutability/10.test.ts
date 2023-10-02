dbd32a9c3c967c8c0209bfb081bb19543a1a9435
IJTINK7 <ijtink7@mail.ru>
2023-10-02 18:22:47 +0300

    Creating some functions with shallow and deep copy in 10th lesson

---
+++ src/10-immutability/10.test.ts
@@ -0,0 +1,55 @@
+import {cutHair, UserWithLaptopType} from "./10";
+
+
+test("Hair length should be decreased", ()=>{
+	const visitor: UserWithLaptopType = {
+		name: "Roma",
+		hairLength: 35,
+		address:{
+			city: "Minsk",
+			house: 12
+		},
+		laptop: {
+			title: "ZenBook"
+		}
+	}
+	const cutUser = cutHair(visitor, 10)
+	visitor.address.city = "Moscow"
+
+	const changeCity = (visitor: UserWithLaptopType, newCity: string) => {
+		return{...visitor, address: {...visitor.address, city: newCity}}
+	}
+
+	const movedUser = changeCity(visitor, "Kiev")
+
+	expect(visitor.hairLength).toBe(35)
+	expect(cutUser.hairLength).toBe(25)
+	expect(cutUser.address).toBe(visitor.address)
+	expect(visitor).toEqual({
+		name: "Roma",
+		hairLength: 35,
+		address:{
+			city: "Moscow",
+			house: 12
+		},
+		laptop: {
+			title: "ZenBook"
+		}
+	})
+	expect(cutUser).toEqual({
+		name: "Roma",
+		hairLength: 25,
+		address:{
+			city: "Moscow",
+			house: 12
+		},
+		laptop: {
+			title: "ZenBook"
+		}
+	})
+	expect(visitor).not.toBe(cutUser)
+	expect(visitor.address).toBe(cutUser.address)
+	expect(visitor.address).not.toBe(movedUser.address)
+	expect(visitor.laptop).toBe(movedUser.laptop)
+	expect(movedUser.address.city).toBe("Kiev")
+})
---
+++ src/10-immutability/10.ts
@@ -0,0 +1,32 @@
+export type VisitorType = {
+	name: string
+	hairLength: number
+	address:{city:string, house: number}
+}
+export type LaptopType={
+	title: string
+}
+export type UserWithLaptopType = VisitorType & {
+	laptop: LaptopType
+}
+const visitor = {
+	name: "Roma",
+	hairLength: 35,
+	address:{
+		city: "Minsk",
+		house: 12
+	}
+}
+export const cutHair = (visitor: VisitorType, length: number) => {
+	return {
+		...visitor,
+		hairLength: visitor.hairLength - length
+	}
+}
+console.log(cutHair(visitor, 10)) //{
+// name: "Roma",
+// 	hairLength: 25,
+// 	address:{
+// 		city: "Minsk"
+// 	}
+// }
